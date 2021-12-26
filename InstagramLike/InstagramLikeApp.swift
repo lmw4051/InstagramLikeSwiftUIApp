32554425aacd9657cb6e64bb3fb3e1cf6879063d
David Lee <david.m.lee@ihwic.com>
2021-12-26 15:33:43 +0800

    Initial Commit

---
+++ InstagramLike/InstagramLikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramLikeApp.swift
+//  InstagramLike
+//
+//  Created by David on 2021/12/26.
+//
+
+import SwiftUI
+
+@main
+struct InstagramLikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
